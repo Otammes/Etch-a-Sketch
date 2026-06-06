5ddeec706d6b9003327f59d17ff26c6be19316bd
Otammes <269725913+Otammes@users.noreply.github.com>
2026-06-06 17:08:46 +0200

    Create 16x16 div grid using for loop

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
+
+function multiplyDivs(){
+    
+    for (i = 0; i <257; i++){
+    const div = document.createElement("div")
+    container.appendChild(div)    
+    }
+    
+}
+
+multiplyDivs()
