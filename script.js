2ec423e67c19d20f1b19ce63a3a6f95904cd5117
Otammes <269725913+Otammes@users.noreply.github.com>
2026-06-07 18:07:39 +0200

    Add hover effect to grid using event listeners

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const container = document.querySelector("#container");
 
 
-function multiplyDivs(){
-    
-    for (i = 0; i <257; i++){
+function createDivs(numberOfDivs){    
+    for (i = 0; i < numberOfDivs; i++){
     const div = document.createElement("div")
+    div.classList = "grid"
     container.appendChild(div)    
     }
-    
-}
+}    
 
-multiplyDivs()
+createDivs(256)
+
+const selectedDivs = container.childNodes;
+
+selectedDivs.forEach((div) => {
+
+    div.addEventListener("mouseenter", (event) => {
+        div.style.backgroundColor = "blue";
+    })
+
+    div.addEventListener("mouseleave", (event) => {
+        div.style.backgroundColor = "white"
+    })
+}
+)
