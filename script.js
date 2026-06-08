c9cb85ba9d72d0e6d1db5df3b59cacb7615f04f8
Otammes <269725913+Otammes@users.noreply.github.com>
2026-06-08 19:36:40 +0200

    Make script functional with new button element and add dynmaic flexbasis property

--- script.js
+++ script.js
@@ -2,28 +2,39 @@ const container = document.querySelector("#container");
 
 
 function createDivs(numberOfDivs){    
-    for (i = 0; i < numberOfDivs; i++){
+    for (let i = 0; i < numberOfDivs; i++){
     const div = document.createElement("div")
     div.classList = "grid"
-    container.appendChild(div)    
-    }
+    const newWidth = 100 / Math.sqrt(numberOfDivs);
+    div.style.flexBasis = `${newWidth}%`
+    container.appendChild(div)
+    div.addEventListener("mouseenter", (event) => {
+    div.style.backgroundColor = "blue";
+    })
+}
 }    
 
 createDivs(256)
 
-const selectedDivs = container.childNodes;
 
-selectedDivs.forEach((div) => {
 
-    div.addEventListener("mouseenter", (event) => {
-        div.style.backgroundColor = "blue";
-    })
-
-}
-)
+let userInput = 0;
 
 const btn = document.querySelector("#btn")
 
 btn.addEventListener("click", (event) => {
-    prompt("How many numbers of squares do you want?")
+    const stringValue = prompt("How many numbers of squares do you want?");
+
+    if (stringValue !== null && stringValue <= 100) {
+        
+        container.replaceChildren() 
+        
+        userInput = parseInt(stringValue);
+        userInput *= userInput
+        createDivs(userInput)
+        }
+    else {
+        alert("Input can not be empty or above 100")
+    }
 })
+
